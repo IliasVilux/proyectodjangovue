a9f7ae87dd1ef29f21db5bf0dfc49f2c9420d2fe
IliasVilux <iliasotsman@gmail.com>
2023-12-28 23:05:42 +0100

    show details

--- my-app/src/routes/colaboradores/[dniCifColaborador]/+page.server.js
+++ my-app/src/routes/colaboradores/[dniCifColaborador]/+page.server.js
@@ -1,31 +1,8 @@
-// import { request, gql } from 'graphql-request';
+export function load({params}){
+    return params.dniCifColaborador ? {dniCifColaborador: params.dniCifColaborador} : null;
+}
 
-// export async function load({ params }) {
-// 	if (params.dniCifcolaborador) {
-// 		const query = gql`
-// 			query colaborador($dniCifColaborador: String!) {
-// 				colaborador(dniCifColaborador: $dniCifColaborador) {
-// 					dniCifColaborador
-// 					nombreColaborador
-// 					apellidosColaborador
-// 				}
-// 			}
-// 		`;
-
-// 		const variables = { dniCifColaborador: params.dniCifColaborador };
-
-// 		const colaborador = await request('http://127.0.0.1:8000/graphql/', query, variables).then(
-// 			(data) => {
-// 				return { colaborador: data.colaborador };
-// 			}
-// 		);
-
-// 		return colaborador.colaborador;
-// 	}
-// 	return null;
-// }
-
-// export const actions = {
-// 	create: () => {},
-// 	update: () => {}
-// };
+export const actions = {
+	create: () => {},
+	update: () => {}
+};
--- my-app/src/routes/colaboradores/[dniCifColaborador]/+page.svelte
+++ my-app/src/routes/colaboradores/[dniCifColaborador]/+page.svelte
@@ -1,30 +1,62 @@
-<!-- <script>
-    export let data;
-    const colaborador = {
-        dniCifColaborador: '',
-        nombreColaborador: ''
-    }
-
-    $: {
-        colaborador.dniCifColaborador = data.dniCifColaborador || colaborador.dniCifColaborador;
-        colaborador.nombreColaborador = data.nombreColaborador || colaborador.nombreColaborador;
-    }
-
-    async function handleSubmit(){
-        console.log(colaborador)
-    }
-</script>
-
-<form method="POST" on:submit|preventDefault={handleSubmit}>
-    <input type="text" name="dni" bind:value={colaborador.dniCifColaborador} />
-    <input type="text" name="name" value={colaborador.nombreColaborador ? colaborador.nombreColaborador : ''}>
-
-    {#if !data.dniCifColaborador}
-        <button type="submit" disabled={colaborador.dniCifColaborador.length < 1}>Crear</button>
-    {:else}
-        <button type="submit">Modificar</button>
-    {/if}
-</form> -->
-
 <script>
+	import {
+		Client,
+		cacheExchange,
+		fetchExchange,
+		setContextClient,
+		queryStore,
+		mutationStore,
+		gql,
+		getContextClient
+	} from '@urql/svelte';
+
+	const client = new Client({
+		url: 'http://127.0.0.1:8000/graphql/',
+		exchanges: [cacheExchange, fetchExchange]
+	});
+	setContextClient(client);
+
+	export let data;
+
+    let colaborador = queryStore({
+		client: getContextClient(),
+		query: gql`
+			query colaboradores($dniCifColaborador: String!) {
+				colaborador(dniCifColaborador: $dniCifColaborador) {
+					dniCifColaborador
+					nombreColaborador
+				}
+			}
+		`,
+		variables: { dniCifColaborador: data.dniCifColaborador }
+	});
+
+	async function handleSubmit() {
+		console.log($colaborador.data.colaborador);
+	}
 </script>
+
+{#if $colaborador.fetching}
+	<p>Loading...</p>
+{:else if $colaborador.error}
+	<p>Oh no... <b>{$colaborador.error.message}</b></p>
+{:else}
+	<form method="POST" on:submit|preventDefault={handleSubmit}>
+		<input type="text" name="dni" bind:value={$colaborador.data.colaborador.dniCifColaborador} />
+		<input
+			type="text"
+			name="name"
+			value={$colaborador.data.colaborador.nombreColaborador
+				? $colaborador.data.colaborador.nombreColaborador
+				: ''}
+		/>
+
+		{#if !data.dniCifColaborador}
+			<button type="submit" disabled={$colaborador.data.colaborador.dniCifColaborador.length < 1}
+				>Crear</button
+			>
+		{:else}
+			<button type="submit">Modificar</button>
+		{/if}
+	</form>
+{/if}
